8c1d105eefe119063a1bd86e06515feb84ec392b
DavidBitencourt <38070806+DavidBitencourt@users.noreply.github.com>
2020-06-07 04:33:26 -0300

    mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -8,7 +8,7 @@ class ItemsControllers {
       return {
         id: item.id,
         title: item.title,
-        image_url: `uploads/${item.image}`,
+        image_url: `http://192.168.0.35:3333/uploads/${item.image}`,
       };
     });
     return response.json(serializedItems);
